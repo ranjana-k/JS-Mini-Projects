1860c3908bf60d3337980b9bf2bf15c5ac2b72a3
Ranjana <ranjanaKumari0710@gmail.com>
2023-03-29 01:53:19 +0530

    Push Notification App- updated code using data-attribute

--- push-notification-app/script.js
+++ push-notification-app/script.js
@@ -1,25 +1,26 @@
 //Model
-const countInc = () => {
-  count = count + 1;
-  return display();
-};
-
-const display = () => {
-  notificationCount.innerText = count;
+const addCountSetter = (countAdd) => {
+  notificationCount.setAttribute("data-count", countAdd + 1);
+  if (countAdd === 0) {
+    notificationCount.classList.add("add-numb");
+  }
 };
 
 //View
 let count = 0;
-const notify = document.querySelector(".notify");
+const notifyBtn = document.querySelector(".notify-btn");
 const notificationCount = document.querySelector(".notification-count");
-display();
 
 //Controller
-notify.addEventListener("click", () => {
-  return countInc();
+notifyBtn.addEventListener("click", () => {
+  let addCount = Number(notificationCount.getAttribute("data-count")) || count;
+  addCountSetter(addCount);
 });
 
+//clearing the notification
 notificationCount.addEventListener("click", () => {
-  count = 0;
-  return display();
+  notificationCount.setAttribute("data-count", count);
+  if (count === 0) {
+    notificationCount.classList.remove("add-numb");
+  }
 });
